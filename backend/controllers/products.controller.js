f23e3ae2cfc350016022c54dba0a90a8dada470d
ASHUTOSH RAJ <ashutoshraj0420@gmail.com>
2023-01-19 04:29:24 +0530

    update product

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -1,3 +1,57 @@
-exports.getAllProducts = (req,res)=>{
-    res.status(200).json({msg:"Running good"})
+const { ProductModel } = require("../models/product.model")
+
+
+
+// create products --Admin
+const createProduct = async (req,res,next)=>{
+    const product = await ProductModel.create(req.body)
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+// get all products
+
+const getAllProducts = async (req,res)=>{
+
+    const products = await ProductModel.find()
+    res.status(200).json({
+        success:true,
+        products
+    })
+}
+
+
+// update product --Admin
+
+const updateProduct = async (req,res)=>{
+
+    let product = ProductModel.findById(req.params.id)
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+    product = await ProductModel.findByIdAndUpdate(req.params.id, req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    })
+    res.status(200).json({
+        success:true,
+        product
+    })
+}
+
+// delete Product
+
+
+module.exports={
+    createProduct,
+    getAllProducts,
+    updateProduct
 }
--- backend/routes/products.route.js
+++ backend/routes/products.route.js
@@ -1,9 +1,15 @@
 const express = require("express")
-const { getAllProducts } = require("../controllers/products.controller")
+const { getAllProducts, createProduct, updateProduct } = require("../controllers/products.controller")
 
 const router = express.Router()
 
 
-router.route("/products").get(getAllProducts)
+router.route("/product").get(getAllProducts)
+
+router.route("/product/new").post(createProduct)
+
+router.route("/product/:id").put(updateProduct)
+
+
 
 module.exports =router
